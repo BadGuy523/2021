955437498fe8db5ce4d0ceb7313cf9dd58a14ddb
BadGuy523 <694255828@qq.com>
2021-07-24 00:34:22 +0800

    SringCloudConfig&Bus测试demo

--- springcloud/代码/springclouddemo/ribbon-client/src/main/java/com/demo/ribbonclient/DemoController.java
+++ springcloud/代码/springclouddemo/ribbon-client/src/main/java/com/demo/ribbonclient/DemoController.java
@@ -1,27 +1,34 @@
 package com.demo.ribbonclient;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@RefreshScope
 @RestController
 public class DemoController {
 
-    @Autowired
-    private RestTemplate restTemplate;
+    @Value("${client.name}")
+    private String name;
 
-    @Bean
-    @LoadBalanced
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+//    @Autowired
+//    private RestTemplate restTemplate;
+
+//    @Bean
+//    @LoadBalanced
+//    public RestTemplate restTemplate() {
+//        return new RestTemplate();
+//    }
 
     @GetMapping("/test")
     public String test(){
-        return restTemplate.getForObject("http://ribbon-server/test",String.class);
+        return "name:" + name;
+//        return restTemplate.getForObject("http://ribbon-server/test",String.class);
     }
 
 }
--- springcloud/代码/springclouddemo/ribbon-server/src/main/java/com/demo/DemoController.java
+++ springcloud/代码/springclouddemo/ribbon-server/src/main/java/com/demo/DemoController.java
@@ -2,17 +2,23 @@ package com.demo;
 
 import com.demo.api.DemoApi;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RestController;
 
+// 用于刷新config服务端获取的配置，需要引入actuator,并配置文件中暴露refresh端点，手动调用http://localhost:8080/actuator/refresh才可刷新
+@RefreshScope
 @RestController
 public class DemoController implements DemoApi {
 
     @Value("${server.port}")
     private int port;
 
+    @Value("${server.name}")
+    private String name;
+
     @Override
     public String test() {
-        return "访问端口："+port;
+        return "name："+name;
     }
 
 }
