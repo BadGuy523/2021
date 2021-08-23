cc08e4339191c4ab9a81c6fa9cb4747630562776
BadGuy523 <694255828@qq.com>
2021-08-24 00:17:23 +0800

    no3

---
+++ leetcode/code/src/com/algorithm/tobeclassified/ProblemNo3.java
@@ -0,0 +1,38 @@
+package com.algorithm.tobeclassified;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+  * @Description: leetcodeNo3-无重复字符的最大子序列长度
+  * @Author: zhangjunqiang
+  * @Date: 2021/8/23 23:16
+  * @version v1.0
+  */
+public class ProblemNo3 {
+
+    public static int lengthOfLongestSubstring(String s) {
+        char[] chars = s.toCharArray();
+        // 记录最大值
+        int maxCount = 0;
+        // 作为滑动窗口进行比较
+        List<Character> container = new ArrayList<>();
+        for (char aChar : chars) {
+            // 如果有重复，需要删除滑动窗口中，重复字符及之前的字符
+            if (container.contains(aChar)) {
+                maxCount = Math.max(container.size(), maxCount);
+                int deleteIndex = container.indexOf(aChar);
+                for (int i = 0; i <= deleteIndex; i++) {
+                    container.remove(0);
+                }
+            }
+            container.add(aChar);
+        }
+        return Math.max(container.size(), maxCount);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("pwwkew"));
+    }
+
+}
