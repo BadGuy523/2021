1054c67e7b525620916ec3731528d9497bf44e44
BadGuy523 <694255828@qq.com>
2022-03-14 16:48:55 +0800

    dubbo集成springcloud以及springboot项目修改

--- SpringCloudAlibaba/代码/spring-boot-dubbo-demo/spring-boot-dubbo-demo-provider/src/main/java/com/demo/springboot/dubbo/provider/ProviderRunner.java
+++ SpringCloudAlibaba/代码/spring-boot-dubbo-demo/spring-boot-dubbo-demo-provider/src/main/java/com/demo/springboot/dubbo/provider/ProviderRunner.java
@@ -1,10 +1,10 @@
 package com.demo.springboot.dubbo.provider;
 
-import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@DubboComponentScan(basePackages = "com.demo.springboot.dubbo.provider.service") // 可行
+// 配置文件已经指定扫描路径
+// @DubboComponentScan(basePackages = "com.demo.springboot.dubbo.provider.service")
 @SpringBootApplication
 public class ProviderRunner {
     public static void main(String[] args) {
