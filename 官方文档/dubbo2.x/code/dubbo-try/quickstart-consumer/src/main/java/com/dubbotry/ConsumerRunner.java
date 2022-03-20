35803af9be5be2fcd477f979ff994905bb99bf8f
BadGuy523 <694255828@qq.com>
2022-03-20 21:40:43 +0800

    dubbo应用名配置问题修复，TODO：无配置中心如何调用

--- 官方文档/dubbo2.x/code/dubbo-try/quickstart-consumer/src/main/java/com/dubbotry/ConsumerRunner.java
+++ 官方文档/dubbo2.x/code/dubbo-try/quickstart-consumer/src/main/java/com/dubbotry/ConsumerRunner.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Runner {
+public class ConsumerRunner {
     public static void main(String[] args) {
-        SpringApplication.run(Runner.class,args);
+        SpringApplication.run(ConsumerRunner.class,args);
     }
 }
