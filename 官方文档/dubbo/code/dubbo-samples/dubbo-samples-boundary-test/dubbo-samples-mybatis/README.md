0beb93d8d96e513dd08f5b30203923b34e28b396
BadGuy523 <694255828@qq.com>
2022-03-20 14:55:37 +0800

    dubbo2.x入门

--- 官方文档/dubbo/code/dubbo-samples/dubbo-samples-boundary-test/dubbo-samples-mybatis/README.md
+++
@@ -1,6 +0,0 @@
-# Dubbo + Mybatis
-
-This is a dubbo + mybatis sample.
-
-In this sample, provider is using @ComponentScan and consumer is using xml configuration.
-
--- 官方文档/dubbo/code/dubbo-samples/dubbo-samples-triple/src/test/java/org/apache/dubbo/sample/tri/GrpcInterOPTest.java
+++
@@ -1,7 +0,0 @@
-package org.apache.dubbo.sample.tri;
-
-/**
- * See <a href="https://grpc.github.io/grpc/cpp/md_doc_http2-interop-test-descriptions.html">https://grpc.github.io/grpc/cpp/md_doc_http2-interop-test-descriptions.html</a>
- */
-public class GrpcInterOPTest {
-}
